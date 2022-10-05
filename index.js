1b6e56916f2b54666911ec2945e20b0dbbbf14be
Adit0507 <adi05072002@gmail.com>
2022-10-05 21:00:26 +0530

    Implemented reducer

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ function orderCake() {
         quantity: 1,
     }
 }
+
+const initialState = {
+    numOfCakes: 10,
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CAKE_ORDERED:
+            return {
+                ...state,   // If the state object contains more than 1 property
+                numOfCakes: state.numOfCakes - 1
+            }
+        default:
+            return state
+    }
+}
+// (previousState, action) => newState
