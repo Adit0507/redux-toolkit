02a10921bfb100ebcb28b29eabe4b8ab18a1a7f6
Adit0507 <adi05072002@gmail.com>
2022-10-12 17:28:24 +0530

    Added an IceCream feature

--- Rtk-Demo/index.js
+++ Rtk-Demo/index.js
@@ -1,5 +1,6 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
 
 console.log('Initial State ', store.getState());
 
@@ -12,4 +13,9 @@ store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.restocked(3))
 
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.restocked(4))
+// 22
 unsubscribe()
